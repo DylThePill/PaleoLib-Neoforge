9d248002220e18a037780271c2eb40f240efdcbd
x29Naybla <29Naybla@gmail.com>
2024-10-24 23:31:50 -0300

    Multiple visual fixes

--- src/main/java/com/x29naybla/paleolib/event/ClientSetUpEvents.java
+++ src/main/java/com/x29naybla/paleolib/event/ClientSetUpEvents.java
@@ -3,6 +3,9 @@ package com.x29naybla.paleolib.event;
 import com.x29naybla.paleolib.PaleoLib;
 import com.x29naybla.paleolib.block.entity.ChiselableBlockRenderer;
 import com.x29naybla.paleolib.block.entity.ModBlockEntityTypes;
+import com.x29naybla.paleolib.item.ModItems;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -14,5 +17,8 @@ public class ClientSetUpEvents {
     @SubscribeEvent
     public static void onRegisterRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(ModBlockEntityTypes.CHISELABLE.get(), ChiselableBlockRenderer::new);
+        ItemProperties.register(ModItems.CHISEL.get(),
+                ResourceLocation.withDefaultNamespace("chiseling"),
+                (p_272332_, p_272333_, p_272334_, p_272335_) -> p_272334_ != null && p_272334_.getUseItem() == p_272332_ ? (float) (p_272334_.getUseItemRemainingTicks() % 10) / 10.0F : 0.0F);
     }
 }
--- src/main/java/com/x29naybla/paleolib/item/ModItems.java
+++ src/main/java/com/x29naybla/paleolib/item/ModItems.java
@@ -11,7 +11,7 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(PaleoLib.MOD_ID);
 
     public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
-            () -> new ChiselItem(new Item.Properties()));
+            () -> new ChiselItem(new Item.Properties().stacksTo(1).durability(64)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
